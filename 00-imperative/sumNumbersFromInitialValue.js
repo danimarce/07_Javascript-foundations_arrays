d9829eb45d207b1fbc649c8c4ec0d70a54d8c83b
DMG <daniel.marce@mataro.epiaedu.cat>
2025-01-27 20:34:25 +0100

    feat: make tests and solve exercise sumNumbersFromInitialValue imperative

--- 00-imperative/sumNumbersFromInitialValue.js
+++ 00-imperative/sumNumbersFromInitialValue.js
@@ -11,3 +11,17 @@
  * @param {number} initialAccumulatedValue - The initial value to start the sum from.
  * @returns {number} The sum of the numbers in the array starting from the initial value.
  */
+export default function sumNumbersFromInitialValue(
+  numbersList,
+  initialAccumulatedValue,
+) {
+  const numbersListLength = numbersList.length;
+
+  for (let numberIndex = 0; numberIndex < numbersListLength; numberIndex++) {
+    const number = numbersList[numberIndex];
+
+    initialAccumulatedValue += number;
+  }
+
+  return initialAccumulatedValue;
+}
