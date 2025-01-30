d8c99e84e68805266a5a7efdafb5ec77a7cdaf13
DMG <daniel.marce@mataro.epiaedu.cat>
2025-01-30 20:21:48 +0100

    make test and solve exercise transformSecondsToWords map

--- 01-map/transformSecondsToWords.test.js
+++ 01-map/transformSecondsToWords.test.js
@@ -1,17 +1,64 @@
-const SECONDS = [2, 5, 100];
-const EMPTY_SECONDS_LIST = [];
-const SINGLE_SECOND_LIST = [42];
-const SINGLE_SECONDS_LIST_WITH_VALUE_ZERO = [0];
-const SECONDS_LIST_WITH_NEGATIVE_VALUES = [-1, -60, -3600];
+import { describe, it, expect } from "vitest";
+import transformSecondsToWords from "./transformSecondsToWords";
 
-describe("transformSecondsToWords", () => {
-  it.todo("should transform an array of seconds to an array of strings");
+describe("Given method transformSecondsToWords", () => {
+  it("When array of numbers is recived Then should return an array of strings", () => {
+    //Arrange
+    const SECONDS = [2, 5, 100];
+    const expectedResult = ["2", "5", "100"];
 
-  it.todo("should handle an empty array");
+    //Act
+    const output = transformSecondsToWords(SECONDS);
 
-  it.todo("should handle an array with one element");
+    //Assert
+    expect(output).toEqual(expectedResult);
+  });
 
-  it.todo("should handle an array with zero");
+  it("When an empty array is recived Then should return an empty array", () => {
+    //Arrange
+    const EMPTY_SECONDS_LIST = [];
+    const expectedResult = [];
 
-  it.todo("should handle an array with negative numbers");
+    //Act
+    const output = transformSecondsToWords(EMPTY_SECONDS_LIST);
+
+    //Assert
+    expect(output).toEqual(expectedResult);
+  });
+
+  it("When an array with one element is recived Then should return an array with one string", () => {
+    //Arrange
+    const SINGLE_SECOND_LIST = [42];
+    const expectedResult = ["42"];
+
+    //Act
+    const output = transformSecondsToWords(SINGLE_SECOND_LIST);
+
+    //Assert
+    expect(output).toEqual(expectedResult);
+  });
+
+  it("When an array with zero is recived Then should return an array with string 0", () => {
+    //Arrange
+    const SINGLE_SECONDS_LIST_WITH_VALUE_ZERO = [0];
+    const expectedResult = ["0"];
+
+    //Act
+    const output = transformSecondsToWords(SINGLE_SECONDS_LIST_WITH_VALUE_ZERO);
+
+    //Assert
+    expect(output).toEqual(expectedResult);
+  });
+
+  it("When an array with negative numbers is recived Then should return an array with strings", () => {
+    //Arrange
+    const SECONDS_LIST_WITH_NEGATIVE_VALUES = [-1, -60, -3600];
+    const expectedResult = ["-1", "-60", "-3600"];
+
+    //Act
+    const output = transformSecondsToWords(SECONDS_LIST_WITH_NEGATIVE_VALUES);
+
+    //Assert
+    expect(output).toEqual(expectedResult);
+  });
 });
